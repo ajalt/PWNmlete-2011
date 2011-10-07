d8cb0ee836598c795a370a0e01ff647a66448f46
Narcoeli <ctrl.alt.aj@gmail.com>
2011-10-07 15:48:15 -0300

    Added some server handling. I'm not sure sending the ALIVE works yet.

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
 import SocketServer
+import hashlib
+
+password = '1a3e1f2bc98def7'
+cookiefile = 'cookie.txt'
+cookie = None
+
+class CommandError(Exception): pass
+class IncorrectChecksumError(Exception):pass
 
 class MyTCPHandler(SocketServer.StreamRequestHandler):
+    def write(self, data):
+        print 'Sending:', data
+        self.wfile.write(data
+                         )
     def handle(self):
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
-        for line in self.rfile:
-            print repr(line)
+
         # Likewise, self.wfile is a file-like object used to write back
         # to the client
         #self.wfile.write(self.data.upper())
+        for directive in self.rfile:
+            print 'Received:', directive
+            command, args = [i.strip() for i in directive.split(':', 1)]
+            if command == 'PARTICIPANT_PASSWORD_CHECKSUM':
+                if hashlib.sha1(password).hexdigest() != args.strip():
+                    #raise IncorrectChecksumError()
+                    print 'Checksum does not match:'
+                    print 'Calculated:', repr(hashlib.sha1(password).hexdigest())
+                    print 'Received:', repr(args.strip())
+            elif command == 'REQUIRE':
+                if args == 'ALIVE':
+                    global cookie
+                    if cookie is None:
+                        with open(cookiefile, 'r') as f:
+                            cookie = f.read.strip()
+                    self.write('ALIVE %s' % cookie)
+        
         
 if __name__ == '__main__':
     host, port = 'localhost', 9999
