e6b3c2bcd84a648118ea05e0ef473674be2d5995
Ryan Child <ryan.child@gmail.com>
2011-10-08 02:38:05 -0400

    refactor, also fixing so that cookie file gets written

--- client.py
+++ client.py
@@ -1,63 +1,53 @@
 import socket
-import urllib2
-
-class CommandError(Exception): pass
 
 host, port = 'helios.ececs.uc.edu', 8180
 password = '1a3e1f2bc98def7'
-ident = 'IDENT'
+ident = 'testing54325'
 server_port = 9999
+server_addr = 'localhost' #use ssh tunnel
 cookie = None
 cookiefile = 'cookie.txt'
 
-def handle(data):
+#process message, returns response string if response is needed
+def ProcessMonitorMsg(line):
     global cookie
-    for directive in data.strip().split('\n'):
-        command, args = [i.strip() for i in directive.split(':', 1)]
-        if command == 'REQUIRE':
-            if args == 'IDENT':
-                return 'IDENT %s' % ident
-            if args == 'PASSWORD':
-                return 'PASSWORD ' + password
-            if args == 'HOST_PORT':
-                return 'HOST_PORT %s %s' % (external_ip, server_port)
-            if args == 'ALIVE':
-                if cookie is None:
-                    with open(cookiefile, 'r') as f:
-                        cookie = f.read().strip()
-                return 'ALIVE %s' % cookie
-        if command == 'RESULT':
-            args = args.split()
-            if args[0] == 'PASSWORD':
-                cookie = args[1]
-                with open(cookiefile, 'w') as f:
-                    f.write(cookie)
-        if command == 'COMMAND_ERROR':
-            raise CommandError(args)
+    msgparts = line.split(':')
+    if len(msgparts) != 2:
+        return
+    command, args = [i.strip() for i in msgparts]
+    if command == 'REQUIRE':
+        if args == 'IDENT':
+            return "IDENT %s\n" % ident
+        if args == 'PASSWORD':
+            return "PASSWORD %s\n" % password
+        if args == 'HOST_PORT':
+            return "HOST_PORT %s %s\n" % (server_addr, server_port)
+        if args == 'ALIVE':
+            if cookie is None:
+                with open(cookiefile, 'r') as f:
+                    cookie = f.read().strip()
+            return 'ALIVE %s' % cookie
+    if command == 'RESULT':
+        args = args.split()
+        if args[0] == 'PASSWORD':
+            cookie = args[1]
+            with open(cookiefile, 'w') as f:
+                f.write(cookie)
+                # flush buffer and close file
+                f.close()
 
+# Connect to server and open stream
+sock = socket.create_connection((host, port))
+f = sock.makefile()
 
-if __name__ == '__main__':
-    #find out external ip address for server
-    #external_ip = urllib2.urlopen('http://automation.whatismyip.com/n09230945.asp').read()
-    #use localhost with an ssh tunnel
-    external_ip = 'localhost'
-    
-    # Create a socket (SOCK_STREAM means a TCP socket)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Connect to server and send data
-    sock.connect((host, port))
-    
-    # Receive data from the server and shut down
-    while True:
-        data = sock.recv(1024)
-        print repr(data)
-        if not data.strip():
-            break
-        send_data = handle(data)
-        if send_data is not None:
-            print 'sending:', repr(send_data + '\n')
-            sock.send(send_data + '\n')
-            
-    sock.close()
+while True:
+    line = f.readline().strip()
+    if not line:
+        break
+    print "incoming>>>" + line
+    response = ProcessMonitorMsg(line)
+    if response:
+        print "outgoing>>>" + response.strip()
+        sock.send(response)
 
+sock.close()
