a805569751b25e1019d987dc2e08c9207fb6eae2
Narcoeli <ctrl.alt.aj@gmail.com>
2011-10-10 00:20:46 -0300

    Fixed checksum calculation and updated code to me more pythonic and to follow PEP 8 coding style.

--- server.py
+++ server.py
@@ -5,13 +5,10 @@ import netsecurity
 cookie = None
 host, port = 'localhost', 9999
 
-class CommandError(Exception): pass
-class IncorrectChecksumError(Exception):pass
-
 class MyTCPHandler(SocketServer.StreamRequestHandler):
     def write(self, data):
-        print 'outcoming>>>', data.strip()
-        self.wfile.write(data)
+        print 'outgoing>>>', data.strip()
+        self.wfile.write(data.rstrip('\n') + '\n')
 
     def handle(self):
         # self.rfile is a file-like object created by the handler;
@@ -19,30 +16,28 @@ class MyTCPHandler(SocketServer.StreamRequestHandler):
 
         # Likewise, self.wfile is a file-like object used to write back
         # to the client
-        #self.wfile.write(self.data.upper())
         for directive in self.rfile:
             print 'incoming>>>:', directive.strip()
             command, args = [i.strip() for i in directive.split(':', 1)]
             if command == 'PARTICIPANT_PASSWORD_CHECKSUM':
-                calcchecksum = hashlib.sha1(netsecurity.password).hexdigest()
-                receivedchecksum = args.strip()
-                if calcchecksum != receivedchecksum:
-                    #raise IncorrectChecksumError()
+                calculated_checksum = hashlib.sha1(netsecurity.password.upper()).hexdigest()
+                received_checksum = args.strip()
+                if calculated_checksum != received_checksum:
                     print '***Checksum does not match:'
-                    print "\tCalculated:\t", calcchecksum
-                    print "\tReceived:\t", receivedchecksum
+                    print '\tCalculated:\t', calculated_checksum
+                    print '\tReceived:\t', received_checksum
             elif command == 'REQUIRE':
                 if args == 'IDENT':
-                    self.write("IDENT %s\n" % netsecurity.ident)
+                    self.write('IDENT %s' % netsecurity.ident)
                 elif args == 'QUIT':
-                    self.write("QUIT\n")
+                    self.write('QUIT')
                 elif args == 'ALIVE':
                     global cookie
                     if cookie is None:
                         with open(netsecurity.cookiefile, 'r') as f:
                             cookie = f.read().strip()
-                    self.write("ALIVE %s\n" % cookie)
+                    self.write('ALIVE %s' % cookie)
         
 print 'Server listening on port %s.' % port
-server = SocketServer.TCPServer((host, port), MyTCPHandler)
+server = SocketServer.ThreadingTCPServer((host, port), MyTCPHandler)
 server.serve_forever()
