313a7b41c1741ca7ba19dbbb12aa8771810b8ed0
Narcoeli <ctrl.alt.aj@gmail.com>
2011-10-10 00:20:16 -0300

    Fixed checksum calculation and updated code to me more pythonic and to follow PEP 8 coding style.

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import contextlib
+
 import netsecurity
 
 # use ssh tunnel
@@ -7,45 +9,35 @@ server_addr, server_port = 'localhost', 9999
 cookie = None
 
 #process message, returns response string if response is needed
-def ProcessMonitorMsg(line):
+def process_monitor_msg(line):
     global cookie
-    msgparts = line.split(':')
-    if len(msgparts) != 2:
-        return
-    command, args = [i.strip() for i in msgparts]
+    command, args = [i.strip() for i in line.split(':', 1)]
     if command == 'REQUIRE':
         if args == 'IDENT':
-            return "IDENT %s\n" % netsecurity.ident
+            return 'IDENT %s\n' % netsecurity.ident
         elif args == 'PASSWORD':
-            return "PASSWORD %s\n" % netsecurity.password
+            return 'PASSWORD %s\n' % netsecurity.password
         elif args == 'HOST_PORT':
-            return "HOST_PORT %s %s\n" % (server_addr, server_port)
+            return 'HOST_PORT %s %s\n' % (server_addr, server_port)
         elif args == 'ALIVE':
             if cookie is None:
                 with open(netsecurity.cookiefile, 'r') as f:
                     cookie = f.read().strip()
-            return "ALIVE %s\n" % cookie
+            return 'ALIVE %s\n' % cookie
     elif command == 'RESULT':
         args = args.split()
         if args[0] == 'PASSWORD':
             cookie = args[1]
             with open(netsecurity.cookiefile, 'w') as f:
                 f.write(cookie)
-                # flush buffer and close file
-                f.close()
 
 # Connect to server and open stream
-sock = socket.create_connection((host, port))
-f = sock.makefile()
+with contextlib.closing(socket.create_connection((host, port))) as sock:
+    for line in sock.makefile():
+        print 'incoming>>>', line.strip()
+        response = process_monitor_msg(line)
+        if response:
+            print 'outgoing>>>' + response.strip()
+            sock.send(response)
 
-while True:
-    line = f.readline().strip()
-    if not line:
-        break
-    print "incoming>>>" + line
-    response = ProcessMonitorMsg(line)
-    if response:
-        print "outgoing>>>" + response.strip()
-        sock.send(response)
-
-sock.close()
+#sock.close()
