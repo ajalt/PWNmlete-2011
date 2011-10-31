88c2a675d13cfea5bb256ddc62b5dd8dc1c4f470
Ryan Child <ryan.child@gmail.com>
2011-10-31 17:51:42 -0400

    adding ident, idents db as command line options to client and server, fixing plaintext mode, manual mode

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import socket
 import sys
 import contextlib
 import argparse
-import sys
 import sqlite3
 
 import diffie_hellman
@@ -17,26 +16,29 @@ dbconn = None
 
 class Settings:
     monitor = 'localhost'
-    monitor_port = 8170
+    monitor_port = 8160
     server = 'localhost'
     server_port = 9999
     encrypt = True
     mode = 'normal'
-    identsdbfile = 'idents.db'
-    ident = 'testing54329'
+    identsdbfile = 'testidents.db'
+    ident = 'TESTING54314'
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='0\/3r34sY NetSecurity Client')
     parser.add_argument('--serverport', help='server port', type=int, default=Settings.server_port)
     parser.add_argument('--manual', help='run client in manual mode', action='store_true')
     parser.add_argument('--plaintext', help='do not encrypt lines before sending', action='store_true')
+    parser.add_argument('--ident', help='ident to use', default=Settings.ident)
+    parser.add_argument('--identsdb', help='sqlite3 database for ident lookup', default=Settings.identsdbfile)
     args = parser.parse_args()
-    if args.serverport:
-        Settings.server_port = args.serverport
     if args.plaintext:
         Settings.encrypt = False
     if args.manual:
         Settings.mode = 'manual'
+    Settings.server_port = args.serverport
+    Settings.ident = args.ident.upper()
+    Settings.identsdbfile = args.identsdb
 
 
 def process_monitor_directive(line):
@@ -47,11 +49,14 @@ def process_monitor_directive(line):
         
     directive, args = [i.strip() for i in line.split(':', 1)]
     if directive == 'WAITING' and authcomplete and Settings.mode == 'manual':
-        command = raw_input('Enter server command: ') + '\n'
+        command = raw_input('Enter command: ') + '\n'
         return mycipher.encrypt(command) if mycipher else command
     elif directive == 'REQUIRE':
         if args == 'IDENT':
-            return 'IDENT %s %s\n' % (Settings.ident, util.baseN(mysession.public_key, 32))
+            if Settings.encrypt:
+                return 'IDENT %s %s\n' % (Settings.ident, util.baseN(mysession.public_key, 32))
+            else:
+                return 'IDENT %s\n' % Settings.ident
         elif args == 'PASSWORD':
             command = 'PASSWORD %s\n' % util.getpassword(dbconn, Settings.ident)
             return mycipher.encrypt(command) if mycipher else command
@@ -64,14 +69,14 @@ def process_monitor_directive(line):
     elif directive == 'RESULT':
         if args == 'ALIVE Identity has been verified.':
             authcomplete = True
-        else:
-            args = args.split()
-            if args[0] == 'PASSWORD' or args[0] == 'CHANGE_PASSWORD':
-                cookie = args[1]
-                util.updatecookie(dbconn, Settings.ident, cookie)
-            if args[0] == 'IDENT' and Settings.encrypt:
-                mysession.set_monitor_key(int(args[1], 32))
-                mycipher = karn.Cipher(mysession.shared_secret)
+            return
+        args = args.split()
+        if args[0] == 'PASSWORD' or args[0] == 'CHANGE_PASSWORD':
+            cookie = args[1]
+            util.updatecookie(dbconn, Settings.ident, cookie)
+        if args[0] == 'IDENT' and Settings.encrypt:
+            mysession.set_monitor_key(int(args[1], 32))
+            mycipher = karn.Cipher(mysession.shared_secret)
 
 
 if __name__ == '__main__':
